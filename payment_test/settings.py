46c3ec52089860fec14fd985f795814996a3b4a3
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2021-04-01 22:33:18 +0000

    fix some issues

--- payment_test/settings.py
+++ payment_test/settings.py
@@ -147,24 +147,19 @@ MEDIA_ROOT=os.path.join(BASE_DIR,MEDIA_URL)
 STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')           
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+   
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')           
 
     
