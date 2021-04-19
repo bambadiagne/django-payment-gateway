aaf16213b187ca47e0f72322c244280a718e6e60
bambadiagne <diagne.bambahmadou@gmail.com>
2021-04-19 20:50:08 +0000

    Test first time paydunya payment

--- paymentapp/views.py
+++ paymentapp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password
 import hashlib
 from . import forms
-#import paydunya
+from paydunya import InvoiceItem
+from .paydunya import invoice
 from payment_test import settings
-
+from .models import Product
 def payment_page(request):
  pass
  #   invoice = paydunya.Invoice(settings.store)
@@ -19,3 +20,18 @@ def checked(request):
             print("Cette requête n'a pas été émise par PayDunya")
     except:
         print("Une erreur est survenue")
+def order(request,product_id):
+    product_to_buy=Product.objects.filter(pk=product_id).first()
+    invoice.add_items([InvoiceItem(
+    name=product_to_buy.product_title,
+    quantity=1,
+    unit_price=product_to_buy.product_price,
+    total_price=product_to_buy.product_price,
+    description=product_to_buy.product_description
+  ),])
+    invoice.total_amount=product_to_buy.product_price
+    successful, response = invoice.create()
+    if successful:
+        print(successful)
+        return redirect("home")
+    return response    
